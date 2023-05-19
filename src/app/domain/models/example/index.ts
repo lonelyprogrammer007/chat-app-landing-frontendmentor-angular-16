c1ac4c22a965d029da3f4c3df119f423f24a9513
Luis Guillermo Figueroa Hernandez <lugfigue@bancolombia.com.co>
2023-05-19 09:14:45 -0500

    boilerplate structure atomic design, clean architecture

---
+++ src/app/domain/models/example/index.ts
@@ -0,0 +1,9 @@
+interface ExampleModel {
+  exampleProp1?: string;
+  exampleProp2?: string;
+  exampleProp3?: string;
+  exampleProp4?: string;
+  exampleProp5?: string;
+  exampleProp6?: string;
+  exampleProp7?: string;
+}
